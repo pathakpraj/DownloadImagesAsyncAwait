233d31c250bf536178e18be01e2ed58b8bfe8c0c
PrajaktaPathak <prajakta.coding.email@gmail.com>
2025-02-26 14:27:31 -0500

    Download with Combine added.

--- DownoadImagesAsyncAwait/DownloadImageAsyncImageLoader.swift
+++ DownoadImagesAsyncAwait/DownloadImageAsyncImageLoader.swift
@@ -7,23 +7,38 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 class DownloadImageAsyncImageLoader {
+
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        guard let data = data,
+              let image = UIImage(data: data),
+              let response = response as? HTTPURLResponse,
+             response.statusCode >= 200 && response.statusCode < 300 else {
+            return nil
+        }
+        return image
+    }
+
 
     func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
         if let url = URL(string: "https://picsum.photos/200") {
             let request = URLRequest(url: url)
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let data = data,
-                      let image = UIImage(data: data),
-                      let response = response as? HTTPURLResponse,
-                     response.statusCode >= 200 && response.statusCode < 300 else {
-                    completionHandler(nil, error)
-                    return
-                }
-                completionHandler(image, nil)
+            URLSession.shared.dataTask(with: request) {[weak self] data, response, error in
+                let image = self?.handleResponse(data: data, response: response)
+                completionHandler(image, error)
             }
             .resume()
         }
+    }
+
+    func downloadWithCombine() -> AnyPublisher <UIImage?, URLError> {
+         let url = URL(string: "https://picsum.photos/200")!
+         let request = URLRequest(url: url)
+           let publisher =  URLSession.shared.dataTaskPublisher(for: url)
+                .map(handleResponse)
+                .eraseToAnyPublisher()
+        return publisher
     }
 }
--- DownoadImagesAsyncAwait/DownloadImageAsyncViewModel.swift
+++ DownoadImagesAsyncAwait/DownloadImageAsyncViewModel.swift
@@ -7,16 +7,19 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 class DownloadImageAsyncViewModel: ObservableObject {
     @Published var image: UIImage? = nil
     let loader = DownloadImageAsyncImageLoader()
+    var cancellables = Set<AnyCancellable>()
 
     func fetchImage() {
         // hard code image
         //self.image = UIImage(systemName: "heart.fill")
 
         // download image with escaping closure
+        /*
         loader.downloadWithEscaping { [weak self] image, error in
             DispatchQueue.main.async {
                 if let image = image {
@@ -24,5 +27,23 @@ class DownloadImageAsyncViewModel: ObservableObject {
                 }
             }
         }
+         */
+
+        // download image with combine
+        loader.downloadWithCombine()
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("Success")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { returnedImage in
+                self.image = returnedImage
+            }
+            .store(in: &cancellables)
+
+
     }
 }
