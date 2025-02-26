7dc9d8d70e78738ad8d596534467fce3df60b3af
PrajaktaPathak <prajakta.coding.email@gmail.com>
2025-02-26 12:51:58 -0500

    Initial Commit

---
+++ DownoadImagesAsyncAwait/DownoadImagesAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownoadImagesAsyncAwaitApp.swift
+//  DownoadImagesAsyncAwait
+//
+//  Created by Prajakta Pathak on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DownoadImagesAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
